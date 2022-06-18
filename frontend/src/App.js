fa3f69830553964b198ca35a90adeef9c16296bd
Vikas Bahuguna <vikasedu10@gmail.com>
2022-06-19 00:56:40 +0530

    Created seperate dir for backend(django), frontend(react)

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Header } from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Hello! This is a basic React frontend page with Backend as Django</p>
+    </div>
+  );
+}
+
+export default App;
